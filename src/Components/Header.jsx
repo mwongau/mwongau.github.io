933d5190d69794d8a5ff3f8bb0f640d7956d9bad
M Wong <wongmt15@gmail.com>
2025-02-02 04:14:37 +1100

    2-2-25

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,8 @@
 function Header(props) {
   return (	  
         <div className="bg-sky-100">
-        <h1 className="text-3xl py-2 text-purple-800">{props.name}</h1>
-        <h3 className="text-xl py-2 text-purple-800">{props.sub_title}</h3>
+        <h1 className="text-3xl py-3 text-purple-800">{props.name}</h1>
+        <h3 className="text-xl py-3 text-purple-800">{props.sub_title}</h3>
 	</div>      		
   );
 }
