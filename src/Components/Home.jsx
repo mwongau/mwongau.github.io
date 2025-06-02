d078a4a54db60ce09ecd5ccfa8ef48108a8a7ebc
M Wong <wongmt15@gmail.com>
2025-06-03 06:23:15 +1000

    3-6-25

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,7 +22,7 @@ export default function Home() {
 		<p>Welcome to my personal website. I am a researcher in computer vision & machine 
 		learning with Ph.D in Computer Science, M.Sc & B.Sc(Hons.) in Electrical and Electronic 
 		Engineering. I am also interested in web app development. This site is built by using 
-		React, Vite, Javascript, HTML and Tailwind CSS. This site is hosted by GitHub Pages.</p>					
+		React.js, Vite, Javascript, HTML and Tailwind CSS. This site is hosted by GitHub Pages.</p>					
 		</div>
 		
 		<p className="bg-gray-100 mt-5" style={mystyle}> This site uses cookies to improve your browsing 
