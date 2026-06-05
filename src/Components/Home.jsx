adae2e7e7553e23b4fff1355ed60a28cc56240b5
M Wong <wongmt15@gmail.com>
2026-06-06 04:52:17 +1000

    6-6-26

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
 	<div>
 		<h2>Home</h2>		
 		<div>
-		<p>Welcome to my personal website. I am a researcher in computer vision & machine 
+		<p>Welcome to my personal website. I am a researcher in computer vision and machine 
 		learning with Ph.D in Computer Science, M.Sc & B.Sc(Hons.) in Electrical and Electronic 
 		Engineering. I am also interested in web app development. This site is built by using 
 		React, Vite, React Router, Javascript, HTML and CSS. This site is hosted by GitHub 
